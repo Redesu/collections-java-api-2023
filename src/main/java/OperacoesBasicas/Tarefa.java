1f539e9fb5ef559eaf3a1e259f213abdbef85ac3
Redesu <willyanmaster@gmail.com>
2023-09-19 15:17:14 -0300

    Operacoes basicas com list

--- src/main/java/OperacoesBasicas/Tarefa.java
+++ src/main/java/OperacoesBasicas/Tarefa.java
@@ -1,5 +1,22 @@
 package main.java.OperacoesBasicas;
 
 public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public String toString(){
+        return descricao;
+    }
+
+
+
     
 }
