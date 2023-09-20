f048ccaeab1dc14945578ad9f4e61ea3fd83bb66
Redesu <willyanmaster@gmail.com>
2023-09-20 13:43:01 -0300

    exercicio ordenacao

---
+++ src/main/java/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,50 @@
+package main.java.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class OrdenacaoPessoa {
+
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordernarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        if (!pessoaList.isEmpty()) {
+            Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+            return pessoasPorAltura;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+        ordenacaoPessoa.adicionarPessoa("Pessoa 1", 20, 1.8);
+        ordenacaoPessoa.adicionarPessoa("Pessoa 2", 19, 1.6);
+        ordenacaoPessoa.adicionarPessoa("Pessoa 3", 18, 1.7);
+        ordenacaoPessoa.adicionarPessoa("Pessoa 4", 22, 1.6);
+        ordenacaoPessoa.adicionarPessoa("Pessoa 5", 25, 1.8);
+
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+        System.out.println(ordenacaoPessoa.ordernarPorIdade());
+
+    }
+
+}
