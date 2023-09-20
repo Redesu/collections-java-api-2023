f3521fdb34230a8dcaf29cfff50d48b72696870e
Redesu <willyanmaster@gmail.com>
2023-09-20 15:46:50 -0300

    Operacoes set

---
+++ src/main/java/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,52 @@
+package main.java.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoCodigoConvite(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for (Convidado c : convidadoSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 2);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 3);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 3);
+
+        System.out.println(conjuntoConvidados.contarConvidados());
+        conjuntoConvidados.exibirConvidados();
+        conjuntoConvidados.removerConvidadoCodigoConvite(2);
+        conjuntoConvidados.exibirConvidados();
+
+    }
+
+}
